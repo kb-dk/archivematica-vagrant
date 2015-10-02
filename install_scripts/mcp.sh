5c1cd00a0c0337c3b94126c4d10376960e243f8a
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2015-10-02 16:12:55 +0200

    Set postfix debconf right

--- install_scripts/mcp.sh
+++ install_scripts/mcp.sh
@@ -4,11 +4,12 @@ echo "Install the Archivematica packages (each of these packages can be installe
 
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password root"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password root"
+
 sudo debconf-set-selections <<< "archivematica-mcp-server archivematica-mcp-server/dbconfig-install boolean true"
 sudo debconf-set-selections <<< "archivematica-mcp-server archivematica-mcp-server/mysql/admin-pass password root"
 sudo debconf-set-selections <<< "archivematica-mcp-server archivematica-mcp-server/password-confirm password root"
 
-sudo debconf-set-selections <<< "postfix postfix/main_mailer_type select 'No configuration'"
+sudo debconf-set-selections <<< "postfix postfix/main_mailer_type string 'No configuration'"
 
 sudo apt-get -y install archivematica-mcp-server
 sudo apt-get -y install archivematica-mcp-client
