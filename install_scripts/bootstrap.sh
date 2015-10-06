e5b6604cef0408a92555ea34094e17824963202d
Asger Askov Blekinge <abr@statsbiblioteket.dk>
2015-10-06 12:54:56 +0200

    Hopefully correct script paths now

--- install_scripts/bootstrap.sh
+++ install_scripts/bootstrap.sh
@@ -4,11 +4,11 @@ SCRIPT_DIR=$(dirname $(readlink -f $0))
 
 echo "INSTALLING ARCHIVEMATICA 1.4 PACKAGES (NEW INSTALL)"
 
-$SCRIPT_DIR/repos.sh > /dev/null
+/vagrant/install_scripts/repos.sh > /dev/null
 
-$SCRIPT_DIR/storage.sh > /dev/null
+/vagrant/install_scripts/storage.sh > /dev/null
 
-$SCRIPT_DIR/mcp.sh > /dev/null
-$SCRIPT_DIR/dashboard.sh > /dev/null
+/vagrant/install_scripts/mcp.sh > /dev/null
+/vagrant/install_scripts/dashboard.sh > /dev/null
 
-$SCRIPT_DIR/guide.sh
+/vagrant/install_scripts/guide.sh
